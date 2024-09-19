883320705d1d47b5b6b8129b8a1c4c4fc43171f3
varun <varunkewlani.21@gmail.com>
2024-09-19 17:20:29 +0530

    commit

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ const fs = require('fs');
 
 // TODO: Write the message "Hello, World!" to the file "output.txt"
 
+fs.writeFile('./output.txt', "Hello, World!", (err)=>
+{
+    if(err)
+    {
+        console.log(err)
+        throw err;
+    }
+    console.log("message written")
+})
